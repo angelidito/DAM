0d318854e6958e2fe69390f43dc2acd9793d0870
Mori <64525549+angelidito@users.noreply.github.com>
2021-09-28 12:55:27 +0200

    PMM empezando con kotlin

--- PMM/Proyectos/MyApplication/app/src/main/java/com/example/myapplication/Principal.kt
+++ PMM/Proyectos/MyApplication/app/src/main/java/com/example/myapplication/Principal.kt
@@ -1,5 +1,32 @@
 package com.example.myapplication
 
-fun main(){
-    print("Hello world!\n\nWhat's up man?\n")
+fun main()
+{
+    // Inmutables
+    val a: Int
+    val b = 2
+    // Mutables
+    var c = 2.4
+    var d: Float
+
+    a = 2
+    d = 2.57f
+    funPrintln("Hello world!\nWhat's up man?")
+
+    var juan = Usuario()
+    funPrintln("Nombre: " + juan.nombre)
+    var maria: Usuario?
+
+    maria = Usuario()
+
+    funPrintln("Nombre nulo: " + maria?.nombre?:"desconocido")
+
+    println("$a" + '\n')
 }
+
+// todo
+fun funPrintln(str:String):Unit
+{
+    println(str)
+}
+
