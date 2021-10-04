3ea3b67142cfbd440a52ef2b787e9fc4c2327019
Mori <64525549+angelidito@users.noreply.github.com>
2021-10-04 15:06:09 +0200

    todo del lunes 4

---
+++ Interfaces/Tareas/Tarea10a/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+
+namespace Tarea10a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opt, a, b;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Escoja una opción: ");
+                    Console.WriteLine("\t1. Suma.\n"
+                                    + "\t2. Resta.\n"
+                                    + "\t3. Multiplicación.\n"
+                                    + "\t4. División.\n"
+                                    + "\t5. Módulo.\n"
+                                    + "\t0. Salir.");
+
+                    opt = Int32.Parse(Console.ReadLine());
+                } while (opt < 0 || opt > 5);
+
+                if (opt != 0)
+                {
+                    Console.WriteLine("Escriba el primer nº: ");
+                    a = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Escriba el segundo nº: ");
+                    b = Int32.Parse(Console.ReadLine());
+                }
+                Console.Write("Resultado: ");
+                switch (opt)
+                {
+                    case 1:
+                        Console.WriteLine(a + " + " + b + " = " + (a + b));
+                        break;
+                    case 2:
+                        Console.WriteLine(a + " - " + b + " = " + (a - b));
+                        break;
+                    case 3:
+                        Console.WriteLine(a + " * " + b + " = " + (a * b));
+                        break;
+                    case 4:
+                        Console.WriteLine(a + " / " + b + " = " + (a / b));
+                        break;
+                    case 5:
+                        Console.WriteLine(a + " % " + b + " = " + (a % b));
+                        break;
+                    case 0:
+                        Console.WriteLine("¡Adiós!");
+                        break;
+                    default:
+                        Console.WriteLine("Error inesperado");
+                        break;
+                }
+                Console.WriteLine();
+
+            } while (opt != 0);
+        }
+    }
+}
---
+++ Interfaces/Tareas/Tarea11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuántos alumnos hay?");
+
+            double n = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nota del alumnos nº {0}:", i + 1);
+                sum = sum + Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("{0} / {1} = {2}", sum, n, (sum / n));
+            Console.WriteLine("La media es de {0}.", (sum / n));
+        }
+    }
+}
