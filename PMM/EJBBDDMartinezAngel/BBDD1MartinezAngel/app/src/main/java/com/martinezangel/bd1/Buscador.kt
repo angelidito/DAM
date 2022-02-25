f66ee4fed7b7037b8ffe7fe5082d7d5b238f2e66
Mori <64525549+angelidito@users.noreply.github.com>
2022-02-25 14:23:04 +0100

    viernes 25-2

---
+++ PMM/EJBBDDMartinezAngel/BBDD1MartinezAngel/app/src/main/java/com/martinezangel/bd1/Buscador.kt
@@ -0,0 +1,11 @@
+package com.ammd.bd1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Buscador : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
